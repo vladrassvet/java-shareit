2ced76bc3fdf880064949ef63e03221063071c53
vladrassvet <vladrassvet@yandex.ru>
2023-12-22 15:14:21 +0300

    Added exceptions classes

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.info("Произошла ошибка 400 при сверке данный");
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.info("Произошла ошибка 404 при поиске переданных параметров в хранилище");
+        return new ErrorResponse(e.getMessage());
+    }
+}
